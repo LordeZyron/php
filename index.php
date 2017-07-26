47c5b482e31e714a9ba7e77906674867ecbf9cb8
Daniel <dan_afram@hotmail.com>
2017-07-25 21:37:32 -0300

    Resolvi de verdade o conflito dos echos

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
 
-<<<<<<< HEAD
+
 	echo "Olá mundo! 456";
 
-	echo "Olá Udemy!";
-=======
-	echo "Olá mundo! 2"
-		
-	echo "Olá Hcode";
->>>>>>> 2f771299651b0dea61e653706310d50aee176ae2
+	echo "Olá Udemy e Hcode!";
+
 
 ?>
